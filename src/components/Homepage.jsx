93b5804cf3074c863a61265fad77cbed79903dea
Bobby Gomez <bobizzle@Bobbys-MacBook-Pro.local>
2022-01-11 02:28:46 -0600

    was able to get crypto data

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,6 +4,7 @@ import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import { Cryptocurrencies, News } from '../components'
 
 const { Title } = Typography;
 
@@ -28,6 +29,12 @@ const Homepage = () => {
                 <Title level={2} className="home-title">Top 10 Cryptocurrencies in the world</Title>
                 <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show More</Link></Title>
             </div>
+            <Cryptocurrencies simplified />
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">Latest Crypto News</Title>
+                <Title level={3} className="show-more"><Link ti ="/news">Show More</Link></Title> 
+            </div>
+            <News simplified />
         </>
     )
 }
