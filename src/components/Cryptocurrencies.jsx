ca7a46ba6190c9fff413604ef0e41c1a891a16cc
Bobby Gomez <bobizzle@Bobbys-MacBook-Pro.local>
2022-01-11 03:09:06 -0600

    filtered cryptos and added search bar

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import millify from 'millify';
 import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input } from 'antd';
@@ -6,16 +6,28 @@ import { Card, Row, Col, Input } from 'antd';
 import { useGetCryptosQuery } from '../services/cryptoApi';
 
 
-const Cryptocurrencies= () => {
-    const { data: cryptosList, isFetching } = useGetCryptosQuery();
-    const [cryptos, setCryptos] = useState(cryptosList?.data?.coins)
+const Cryptocurrencies= ({ simplified }) => {
+    const count = simplified ? 10 : 100;
+    const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
+    const [cryptos, setCryptos] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    useEffect(() => {
+        const filteredData = cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
+        setCryptos(filteredData);
+
+    }, [cryptosList, searchTerm]);
     
-    console.log(cryptos);
+   if(isFetching) return 'Loading ...';
 
     return (
         <>
-            <Row gutters={[32, 32]} className="crypto-card-container">
-                {cryptos.map((currency) => (
+            <div className="search-crypto">
+                <Input placeHolder="Search Cryptocurrency" onChange={(e) => setSearchTerm(e.target.value)} />
+            </div>
+            <Row gutter={[32, 32]} className="crypto-card-container">
+                {cryptos?.map((currency) => (
                     <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
                         <Link to={`/crypto/${currency.id}`}>
                             <Card
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -9,7 +9,7 @@ import { Cryptocurrencies, News } from '../components'
 const { Title } = Typography;
 
 const Homepage = () => {
-    const { data, isFetching } = useGetCryptosQuery();
+    const { data, isFetching } = useGetCryptosQuery(10);
     const globalStats = data?.data?.stats;
     console.log(data);
 
