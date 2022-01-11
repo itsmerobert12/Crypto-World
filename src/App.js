0d8b4efee2e87f86371a0fe814b248632d8d72a4
Bobby Gomez <bobizzle@Bobbys-MacBook-Pro.local>
2022-01-10 23:31:44 -0600

    footer adjustments

--- src/App.js
+++ src/App.js
@@ -33,9 +33,17 @@ const App = () => {
                         </Switch>
                     </div>
                 </Layout>
-            </div>
             <div className="footer">
-
+                <Typography.Title level={5} style={{ color: 'white', textAlign: 'center'}}>
+                    Crypto-World <br />
+                    All rights reserved
+                </Typography.Title>
+                <Space>
+                    <Link to="/">Home</Link>
+                    <Link to="/exchanges">Exchanges</Link>
+                    <Link to="/news">News</Link>
+                </Space>
+                </div>
             </div>
 
         </div>
