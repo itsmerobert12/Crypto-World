7468380e5cc3aa19dded39d9715d09b36f23c41d
Bobby Gomez <bobizzle@Bobbys-MacBook-Pro.local>
2022-02-03 18:41:23 -0600

    redeploying

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import millify from 'millify';
 import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input } from 'antd';
+// webpack hell again
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
 
